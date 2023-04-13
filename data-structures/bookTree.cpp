3e738c6a09c298b240e204475471ce99d801ff18
SK <sarthakkarandikar03@gmail.com>
2023-04-13 16:41:40 +0000

    Apr 13, 2023, 10:11 PM

--- data-structures/bookTree.cpp
+++ data-structures/bookTree.cpp
@@ -12,101 +12,101 @@ class Book
     public: BTNode *root, *temp;
     char d[20];
     Book()
- {
- temp = root = NULL;
- }
- void create()
- {
- char ans='y';
- do
- {
- temp = new BTNode();
- cout<<"\nEnter The Element:";
- cin>>temp->data;
- temp->left=NULL;
- temp->right=NULL;
- if(root == NULL)
- {
- root = temp;
- }
- else
- {
- insert(root, temp);
- }
- cout<<"\nDo You Want to Enter More Elements?(y/n)";
- cin>>ans;
- }while(ans == 'y'|| ans == 'Y');
- }
- void insert(BTNode *root, BTNode *temp)
- {
- char ch;
- cout<<"\nWhere to insert left/right?"<<root->data<<":";
- cin>>ch;
- if(ch == 'r' || ch == 'R')
- {
- if(root->right == NULL)
- {
- root->right = temp;
- }
- else
- insert(root->right, temp);
- }
- else
- {
- if(root->left == NULL)
- {
- root->left = temp;
- }
- else
- insert(root->left, temp);
- }
- }
- void rec_inorder(BTNode *root)
- {
- if(root!=NULL)
- {
- rec_inorder(root->left);
- cout<<root->data;
- rec_inorder(root->right);
- }
- }
- void printLevelOrder(BTNode *root)
- {
- int h = height(root);
- int i;
- for(i=1;i<=h;i++)
- {
- cout<<"\n";
- printLevel(root,i);
- }
- }
- void printLevel(BTNode *root, int level)
- {
- if(root == NULL)
- return;
- if(level == 1 )
-//
- cout<<" "<<root->data;
- else if (level>1)
- {
- printLevel(root->left,level-1);
- printLevel(root->right,level-1);
- }
- }
- int height(BTNode *node)
- {
- if(node == NULL)
- return 0;
- else
- {
- int lheight = height(node->left);
- int rheight = height(node->right);
- if(lheight>rheight)
- return (lheight+1);
- else
- return (rheight+1);
- }
- }
+    {
+        temp = root = NULL;
+    }
+    void create()
+    {
+    char ans='y';
+    do
+    {
+    temp = new BTNode();
+    cout<<"\nEnter The Element:";
+    cin>>temp->data;
+    temp->left=NULL;
+    temp->right=NULL;
+    if(root == NULL)
+    {
+    root = temp;
+    }
+    else
+    {
+    insert(root, temp);
+    }
+    cout<<"\nDo You Want to Enter More Elements?(y/n)";
+    cin>>ans;
+    }while(ans == 'y'|| ans == 'Y');
+    }
+    void insert(BTNode *root, BTNode *temp)
+    {
+    char ch;
+    cout<<"\nWhere to insert left/right?"<<root->data<<":";
+    cin>>ch;
+    if(ch == 'r' || ch == 'R')
+    {
+    if(root->right == NULL)
+    {
+    root->right = temp;
+    }
+    else
+    insert(root->right, temp);
+    }
+    else
+    {
+    if(root->left == NULL)
+    {
+    root->left = temp;
+    }
+    else
+    insert(root->left, temp);
+    }
+    }
+    void rec_inorder(BTNode *root)
+    {
+    if(root!=NULL)
+    {
+    rec_inorder(root->left);
+    cout<<root->data;
+    rec_inorder(root->right);
+    }
+    }
+    void printLevelOrder(BTNode *root)
+    {
+    int h = height(root);
+    int i;
+    for(i=1;i<=h;i++)
+    {
+    cout<<"\n";
+    printLevel(root,i);
+    }
+    }
+    void printLevel(BTNode *root, int level)
+    {
+    if(root == NULL)
+    return;
+    if(level == 1 )
+    //
+    cout<<" "<<root->data;
+    else if (level>1)
+    {
+    printLevel(root->left,level-1);
+    printLevel(root->right,level-1);
+    }
+    }
+    int height(BTNode *node)
+    {
+    if(node == NULL)
+    return 0;
+    else
+    {
+    int lheight = height(node->left);
+    int rheight = height(node->right);
+    if(lheight>rheight)
+    return (lheight+1);
+    else
+    return (rheight+1);
+    }
+    }
 };
 int main()
  {
