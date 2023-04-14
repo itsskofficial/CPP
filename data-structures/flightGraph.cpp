fb89f96805eafa1ad84ab72b3cdd78590eb84ece
SK <sarthakkarandikar03@gmail.com>
2023-04-14 14:03:57 +0000

    Apr 14, 2023, 7:33 PM

--- data-structures/flightGraph.cpp
+++ data-structures/flightGraph.cpp
@@ -1,7 +1,3 @@
-/*
-Problem statement: There are flight paths between cities. If there is a flight between city A and city B then there is an edge between the cities. The cost of the edge can be the time that flight takes to reach city B from A, or the amount of fuel used for the journey. Represent this as a graph.  The node can be represented by airport name or name of the city. Use adjacency list representation of the graph or use adjacency matrix representation of the graph. 	 
-*/
-
 #include<iostream>
 #include<string.h>
 using namespace std;
@@ -10,7 +6,7 @@ class flight
 { 	 
 	public:
 		int am[10][10];
-           		char city_index[10][10];
+        char city_index[10][10];
 		flight();
 	   	int create();
 	   	void display(int city_count);
