820232b846cb74776d3f88f7f59b3e503f504339
SK <sarthakkarandikar03@gmail.com>
2023-04-16 11:42:40 +0000

    Apr 16, 2023, 5:12 PM

--- data-structures/studentFile.cpp
+++ data-structures/studentFile.cpp
@@ -1,207 +1,204 @@
-#include<iostream>
-#include<fstream>
-#include<cstring>
-using namespace std;
-class tel
- {
-
- public:
- int rollNo,roll1;
- char name[10];
- char div;
- char address[20];
- void accept()
- {
-  cout<<"\n\tEnter Roll Number : ";
-  cin>>rollNo;
-  cout<<"\n\tEnter the Name : ";
-  cin>>name;
-  cout<<"\n\tEnter the Division:";
-  cin>>div;
-  cout<<"\n\tEnter the Address:";
-  cin>>address;
- }
-        void accept2()
-        {
-               cout<<"\n\tEnter the Roll No. to modify : ";
-               cin>>rollNo;
-        }
-        void accept3()
-        {
-              cout<<"\n\tEnter the name to modify : ";
-              cin>>name;
-        }
-        int getRollNo()
-        {
-         return rollNo;
-        }
-  void show()
-  {
-
-  cout<<"\n\t"<<rollNo<<"\t\t"<<name<<"\t\t"<<div<<"\t\t"<<address;
-  }
+#include <iomanip.h>
+#include <iostream.h>
+#include <fstream.h>
+#include <conio.h>
+#include <string.h>
+struct student
+{int rollno;
+char name[20];
+float marks;
+int status;
 };
-
-int main()
+class sequential
+{ char master1[30];
+fstream mas;
+public:
+sequential(char *a)
 {
- int i,n,ch,ch1,rec,start,count,add,n1,add2,start2,n2,y,a,b,on,oname,add3,start3,n3,y1,add4,start4,n4;
- char name[20],name2[20];
- tel t1;
- count=0;
- fstream g,f;
- do
- {
-  cout<<"\n>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<";
-  cout<<"\n1.Insert and overwrite\n2.Show\n3.Search & Edit(number)\n4.Search & Edit(name)\n5.Search & Edit(onlynumber)\n6.Search & edit(only name)\n 7.Delete a Student Record\n 8.Exit\n\tEnter the Choice\t:";
-  cin>>ch;
-  switch(ch)
-  {
-  case 1:
-   f.open("StuRecord.txt",ios::out);
-   x:t1.accept();
-   f.write((char*) &t1,(sizeof(t1)));
-   cout<<"\nDo you want to enter more records?\n1.Yes\n2.No";
-   cin>>ch1;
-    if(ch1==1)
-     goto x;
-    else
-    {
-     f.close();
-     break;
-    }
-
-  case 2:
-   f.open("StuRecord.txt",ios::in);
-   f.read((char*) &t1,(sizeof(t1)));
-   //cout<<"\n\tRoll No.\t\tName \t\t Division \t\t Address";
-   while(f)
-   {
-    t1.show();
-    f.read((char*) &t1,(sizeof(t1)));
-   }
-   f.close();
-   break;
-  case 3:
-   cout<<"\nEnter the roll number you want to find";
-   cin>>rec;
-   f.open("StuRecord.txt",ios::in|ios::out);
-   f.read((char*)&t1,(sizeof(t1)));
-   while(f)
-   {
-    if(rec==t1.rollNo)
-    {
-     cout<<"\nRecord found";
-     add=f.tellg();
-     f.seekg(0,ios::beg);
-           start=f.tellg();
-     n1=(add-start)/(sizeof(t1));
-     f.seekp((n1-1)*sizeof(t1),ios::beg);
-     t1.accept();
-     f.write((char*) &t1,(sizeof(t1)));
-     f.close();
-     count++;
-     break;
-    }
-    f.read((char*)&t1,(sizeof(t1)));
-       }
-   if(count==0)
-          cout<<"\nRecord not found";
-   f.close();
-   break;
-
-  case 4:
-    cout<<"\nEnter the name you want to find and edit";
-    cin>>name;
-   f.open("StuRecord.txt",ios::in|ios::out);
-   f.read((char*)&t1,(sizeof(t1)));
-   while(f)
-   {
-    y=(strcmp(name,t1.name));
-    if(y==0)
-    {
-     cout<<"\nName found";
-     add2=f.tellg();
-     f.seekg(0,ios::beg);
-     start2=f.tellg();
-     n2=(add2-start2)/(sizeof(t1));
-     f.seekp((n2-1)*sizeof(t1),ios::beg);
-     t1.accept();
-     f.write((char*) &t1,(sizeof(t1)));
-     f.close();
-     break;
-    }
-          f.read((char*)&t1,(sizeof(t1)));
-   }
-   break;
-      case 5:
-            cout<<"\n\tEnter the roll number you want to modify";
-            cin>>on;
-            f.open("StuRecord.txt",ios::in|ios::out);
-            f.read((char*) &t1,(sizeof(t1)));
-            while(f)
-            {
-              if(on==t1.rollNo)
-              {
-                cout<<"\n\tNumber found";
-                add3=f.tellg();
-                f.seekg(0,ios::beg);
-                start3=f.tellg();
-                n3=(add3-start3)/(sizeof(t1));
-                f.seekp((n3-1)*(sizeof(t1)),ios::beg);
-                t1.accept2();
-                f.write((char*)&t1,(sizeof(t1)));
-                f.close();
-                break;
-              }
-              f.read((char*)&t1,(sizeof(t1)));
-           }
-           break;
-      case 6:
-            cout<<"\nEnter the name you want to find and edit";
-    cin>>name2;
-   f.open("StuRecord.txt",ios::in|ios::out);
-   f.read((char*)&t1,(sizeof(t1)));
-   while(f)
-   {
-    y1=(strcmp(name2,t1.name));
-    if(y1==0)
-    {
-     cout<<"\nName found";
-     add4=f.tellg();
-     f.seekg(0,ios::beg);
-     start4=f.tellg();
-     n4=(add4-start4)/(sizeof(t1));
-     f.seekp((n4-1)*sizeof(t1),ios::beg);
-     t1.accept3();
-     f.write((char*) &t1,(sizeof(t1)));
-     f.close();
-     break;
-    }
-          f.read((char*)&t1,(sizeof(t1)));
-   }
-   break;
-    case 7:
-      int roll;
-      cout<<"Please Enter the Roll No. of Student Whose Info You Want to Delete: ";
-     cin>>roll;
-     f.open("StuRecord.txt",ios::in);
-     g.open("temp.txt",ios::out);
-     f.read((char *)&t1,sizeof(t1));
-     while(!f.eof())
-     {
-        if (t1.getRollNo() != roll)
-           g.write((char *)&t1,sizeof(t1));
-         f.read((char *)&t1,sizeof(t1));
-     }
-    cout << "The record with the roll no. " << roll << " has been deleted " << endl;
-     f.close();
-     g.close();
-     remove("StuRecord.txt");
-     rename("temp.txt","StuRecord.txt");
-      break;
-    case 8:
-       cout<<"\n\tThank you";
-       break;
-        }
-  } while(ch!=8);
+strcpy(master1,a);
+mas.open(master1,ios::binary|ios::in);
+if(mas.fail())
+mas.open(master1,ios::binary | ios::out);
+mas.close();
 }
+void read(); //dispaly master file void insert(student rec1);
+int Delete(int rollno);
+int search(int rollno);
+void pack();
+void update();
+void display(int recno) // display a particular record
+{ student rec1;
+mas.open(master1,ios::binary | ios::in | ios::nocreate);
+mas.seekg(recno*sizeof(student),ios::beg);
+mas.read((char*) &rec1,sizeof(student));
+cout<<“\n”<<rec1.rollno<<“”<<rec1.name<<“”<<setprecision(2)<<rec1.marks;
+mas.close();
+}
+};
+void main()
+{ sequential object(“master.txt”);
+int rollno,op,recno;
+student rec1;
+do
+{ cout<<“\n\n1)Read(Print)\n2)Insert\n3)Delete\n4)Update”;
+cout<<“\n5)Search\n6)Pack\n7)Quit”;
+cout<<“\nEnter Your Choice:”;
+cin>>op;
+switch(op)
+{ case 1: object.read();break; case 2: cout<<“\nEnter a record to be inserted(roll no,name,marks : “;
+cin>>rec1.rollno>>rec1.name>>rec1.marks;
+object.insert(rec1);
+break;
+case 3: cout<<“\nEnter the roll no.:”;
+cin>>rollno;
+object.Delete(rollno);
+break;
+case 4: object.update(); break;
+case 5: cout<<“\nEnter a roll no. : “;
+cin>>rollno;
+recno=object.search(rollno);
+if(recno>=0)
+{ cout<<“\n Record No.: “<< recno;
+object.display(recno);
+}
+else
+cout<<“\nRecord Not Found “;
+break;
+case 6: object.pack();break;
+}
+}while(op!=7);
+}
+void sequential::read()
+{ student crec; int i=1,n;
+cout<<“\n*********Data File*********\n”;
+mas.open(master1,ios::binary | ios::in | ios::nocreate);
+mas.seekg(0,ios::end);/*go to the end of file */
+n=mas.tellg()/sizeof(student);
+mas.seekg(0,ios::beg);
+for(i=1;i<=n;i++)
+{ mas.read((char*)&crec,sizeof(student));
+if(crec.status==0)
+cout<<“\n”<<i<<“) “<<crec.rollno<<“”<<crec.name<<“”<<setprecision(2)<<crec.marks;
+else
+cout<<“\n”<<i<<“) “<<” ****** deleted *********”;
+}
+mas.close();
+}
+void sequential::insert(student rec1)
+{ student crec;
+int n,i,k;
+mas.open(master1,ios::in | ios::out | ios::nocreate);
+rec1.status=0;
+mas.seekg(0,ios::end);/*go to the end of file */
+n=mas.tellg()/sizeof(student);
+if(n==0)
+{
+mas.write((char*)&rec1,sizeof(student)); mas.close();
+return;
+}
+/* Shift records until the point of insertion */
+i=nÂ­1;
+while(i>=0)
+{ mas.seekg(i*sizeof(student),ios::beg);
+mas.read((char*)&crec,sizeof(student));
+if(crec.rollno>rec1.rollno)
+{ mas.seekp((i+1)*sizeof(student),ios::beg);
+mas.write((char*)&crec,sizeof(student));
+}
+else
+break;
+iÂ­Â­;
+}
+/*insert the record at (i+1)th position */
+i++;
+mas.seekp(i*sizeof(student),ios::beg);
+mas.write((char*)&rec1,sizeof(student));
+mas.close();
+}
+int sequential::Delete(int rollno)
+{ student crec;
+int i,n; mas.open(master1,ios::in | ios::out | ios::nocreate);
+mas.seekg(0,ios::end);/*go to the end of file */
+n=mas.tellg()/sizeof(student);
+mas.seekg(0,ios::beg);
+for(i=0;i<n;i++)
+{ mas.read((char*)&crec,sizeof(student));
+if(crec.status==0)
+{
+if(crec.rollno>rollno)
+{cout<<“\nRecord does not exist …”;
+mas.close();
+return(0);
+}
+if(crec.rollno==rollno)
+{crec.status=1;
+mas.seekp(i*sizeof(student),ios::beg);
+mas.write((char*)&crec,sizeof(student));
+mas.close();
+return(1);
+}
+}
+}
+return(0);
+}
+int sequential::search(int rollno){ student crec;
+int i,n;
+mas.open(master1,ios::in | ios::out | ios::nocreate);
+mas.seekg(0,ios::end);/*go to the end of file */
+n=mas.tellg()/sizeof(student);
+mas.seekg(0,ios::beg);
+for(i=0;i<n;i++)
+{ mas.read((char*)&crec,sizeof(student));
+if(crec.status==0)
+{
+if(crec.rollno>rollno)
+{mas.close();
+return(Â­1);
+}
+if(crec.rollno==rollno)
+{ mas.close();
+return(i);
+}
+}
+}
+return(Â­1);
+}
+void sequential::pack()
+{ fstream temp;
+student crec;int i,n;
+mas.open(master1,ios::binary | ios::in);
+temp.open(“temp.txt”,ios::out | ios::trunc | ios::binary);
+mas.seekg(0,ios::end);/*go to the end of file */
+n=mas.tellg()/sizeof(student);
+mas.seekg(0,ios::beg);
+for(i=0;i<n;i++)
+{ mas.read((char*)&crec,sizeof(student));
+if(crec.status==0)
+temp.write((char*)&crec,sizeof(student));
+}
+mas.close();
+temp.close();
+temp.open(“temp.txt”,ios::binary | ios::in);
+mas.open(master1,ios::binary | ios::out | ios::trunc);
+temp.seekg(0,ios::end);/*go to the end of file */
+n=temp.tellg()/sizeof(student);
+temp.seekg(0,ios::beg);
+for(i=0;i<n;i++)
+{ temp.read((char*)&crec,sizeof(student));
+mas.write((char*)&crec,sizeof(student));
+}
+mas.close();
+temp.close();
+}void sequential::update()
+{ int rollno;
+student rec1;
+cout<<“\n Enter the rollno of the record to be updated : “;
+cin>>rollno;
+cout<<“\nEnter a new record(roll no. name marks : “;
+cin>>rec1.rollno>>rec1.name>>rec1.marks;
+if(Delete(rollno))
+insert(rec1);
+else
+cout<<“\n Record not found :”;
+}
+
