2dea64f3009acf321a40608b3ad9fe95a2cca8ee
SK <sarthakkarandikar03@gmail.com>
2023-04-16 12:18:13 +0000

    Apr 16, 2023, 5:48 PM

--- data-structures/studentFile.cpp
+++ data-structures/studentFile.cpp
@@ -178,18 +178,18 @@ void sequential::pack()
         fp.read((char*)&current,sizeof(student));
         if(current.status==0)
         temp.write((char*)&current,sizeof(student));
-        }
-        fp.close();
-        temp.close();
-        temp.open("temp.txt",ios::binary | ios::in);
-        fp.open(master,ios::binary | ios::out | ios::trunc);
-        temp.seekg(0,ios::end);/*go to the end of file */
-        n=temp.tellg()/sizeof(student);
-        temp.seekg(0,ios::beg);
-        for(i=0;i<n;i++)
-        { temp.read((char*)&current,sizeof(student));
-        fp.write((char*)&current,sizeof(student));
-        }
+    }
+    fp.close();
+    temp.close();
+    temp.open("temp.txt",ios::binary | ios::in);
+    fp.open(master,ios::binary | ios::out | ios::trunc);
+    temp.seekg(0,ios::end);/*go to the end of file */
+    n=temp.tellg()/sizeof(student);
+    temp.seekg(0,ios::beg);
+    for(i=0;i<n;i++)
+    { temp.read((char*)&current,sizeof(student));
+    fp.write((char*)&current,sizeof(student));
+    }
     fp.close();
     temp.close();
 }
