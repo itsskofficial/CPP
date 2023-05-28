4271c86a0470e18cef3efbf8601dfdda2d2ece4d
SK <sarthakkarandikar03@gmail.com>
2023-05-28 03:24:09 +0000

    May 28, 2023, 8:54 AM

--- data-structures/businessGraph.cpp
+++ data-structures/businessGraph.cpp
@@ -70,9 +70,9 @@ class Graph
             u=from[v];
             cout<<data[u]<<"==>"<<data[v]<<"\tcost: "<<AM[u][v]<<endl;
             visited[v]=1;
-            
+
             for(int j=1;j<num;j++){
-                if(visited[j]==0&&AM[j][v]<distance[j])
+                if(visited[j]==0 && AM[j][v]<distance[j])
                 {
                     distance[j]=AM[j][v];
                     from[j]=v;
