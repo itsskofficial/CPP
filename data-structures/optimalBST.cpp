3ab5230188baead24ec939b4d8ed922e194bb261
SK <sarthakkarandikar03@gmail.com>
2023-05-29 10:55:24 +0000

    May 29, 2023, 4:25 PM

--- data-structures/optimalBST.cpp
+++ data-structures/optimalBST.cpp
@@ -1,28 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int sum(int frequency[], int i, int j)
+int sum(int frequency[], int fIndex, int j)
 {
     int sum = 0;
-    for (int x = i; x <= j; x++)
+    for (int x = fIndex; x <= j; x++)
         sum += frequency[x];
     return sum;
 }
 
-int optimalCost(int frequency[], int i, int j)
+int optimalCost(int frequency[], int fIndex, int j)
 {
-    // if (j < i)
+    // if (j < fIndex)
     //     return 0;
-    // if (j == i)
-    //     return frequency[i];
+    // if (j == fIndex)
+    //     return frequency[fIndex];
 
-    int frequencySum = sum(frequency, i, j);
+    int frequencySum = sum(frequency, fIndex, j);
 
     int min = INT_MAX;
 
-    for (int r = i; r <= j; ++ r)
+    for (int r = fIndex; r <= j; ++ r)
     {
-        int cost = optimalCost(frequency, i, r - 1) + optimalCost(frequency, r + 1, j);
+        int cost = optimalCost(frequency, fIndex, r - 1) + optimalCost(frequency, r + 1, j);
         if (cost < min)
             min = cost;
     }
