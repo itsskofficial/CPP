28e2cd096b5e61c6e5ab061844974582a58ef271
SK <sarthakkarandikar03@gmail.com>
2023-05-31 04:02:50 +0000

    May 31, 2023, 9:32 AM

--- data-structures/binarySearchTree.cpp
+++ data-structures/binarySearchTree.cpp
@@ -64,27 +64,27 @@ public:
  
  int search(node *root ,int x)
  {
-  if( root != NULL)
-  {
-   if(root->data == x)
-   {
-    cout<<":-- RECORD FOUND --:"<<endl;
-    return 1;
-   }
-   else if(x < root->data)
-   {
-    this->search(root->left, x);
-   }
-   else if(x > root->data)
-   {
-    this->search(root->right , x);
-   }
-  }
-  else
-  {
-    return 0;
-  }
-  return 2;
+    if( root != NULL)
+    {
+    if(root->data == x)
+    {
+      cout<<":-- RECORD FOUND --:"<<endl;
+      return 1;
+    }
+    else if(x < root->data)
+    {
+      this->search(root->left, x);
+    }
+    else if(x > root->data)
+    {
+      this->search(root->right , x);
+    }
+    }
+    else
+    {
+      return 0;
+    }
+    return 2;
  }
  
  
