d0a7492466311ee8be02b13328308ca5d5fa46ef
SK <sarthakkarandikar03@gmail.com>
2023-05-31 11:01:55 +0000

    May 31, 2023, 4:31 PM

--- data-structures/dictionaryHashing.py
+++ data-structures/dictionaryHashing.py
@@ -7,8 +7,8 @@ class Dictionary:
         if self.elements[pos]['key']==0:
             self.elements[pos]=ele
         else:
-            while(self.elements[pos]['key']==0)
-            pos=(pos+1)%len(self.elements)
+            while(self.elements[pos]['key']==0):
+                pos=(pos+1)%len(self.elements)
 
 
         
