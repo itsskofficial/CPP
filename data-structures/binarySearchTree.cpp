ed90892aa22b2a70678b708996822a14dcb69109
SK <sarthakkarandikar03@gmail.com>
2023-05-31 04:11:49 +0000

    May 31, 2023, 9:41 AM

--- data-structures/binarySearchTree.cpp
+++ data-structures/binarySearchTree.cpp
@@ -17,150 +17,150 @@ class BST
   
   public:
 
-  node * root;
-  BST()
+    node * root;
+    BST()
+    {
+      root = NULL;
+    }
+  
+    node *createNode(int x)
+    {
+      node * ptr = new node();
+      ptr->data = x;
+      ptr->left = NULL;
+      ptr->right = NULL;
+      return ptr;
+    }
+  
+  node *insert( node *root , int x)
   {
-    root = NULL;
+      if( root == NULL )
+      {
+        root = createNode(x);
+      }
+      else if( root->data > x)
+      {
+        root->left = insert(root->left , x);
+      }
+      else
+      {
+        root->right = insert( root->right , x);
+      }
+
+      return root;
   }
- 
-  node *createNode(int x)
+  
+  void input()
   {
-    node * ptr = new node();
-    ptr->data = x;
-    ptr->left = NULL;
-    ptr->right = NULL;
-    return ptr;
+      cout<<"Enter number of elements in the BST : ";
+      cin>>n;
+
+      for(int i = 0 ; i < n ; i++)
+      {
+        cout<<"Data = ";
+        cin>>x;
+        root = insert(root , x);
+      }
   }
- 
- node *insert( node *root , int x)
- {
-    if( root == NULL )
-    {
-      root = createNode(x);
-    }
-    else if( root->data > x)
-    {
-      root->left = insert(root->left , x);
-    }
-    else
-    {
-      root->right = insert( root->right , x);
-    }
-
-    return root;
- }
- 
- void input()
- {
-    cout<<"Enter number of elements in the BST : ";
-    cin>>n;
-
-    for(int i = 0 ; i < n ; i++)
-    {
-      cout<<"Data = ";
-      cin>>x;
-      root = insert(root , x);
-    }
- }
- 
- int search(node *root ,int x)
- {
-    if( root != NULL)
-    {
-      if(root->data == x)
+  
+  int search(node *root ,int x)
+  {
+      if( root != NULL)
       {
-        return 1;
+        if(root->data == x)
+        {
+          return 1;
+        }
+        else if(x < root->data)
+        {
+          this->search(root->left, x);
+        }
+        else if(x > root->data)
+        {
+          this->search(root->right , x);
+        }
       }
-      else if(x < root->data)
-      {
-        this->search(root->left, x);
-      }
-      else if(x > root->data)
-      {
-        this->search(root->right , x);
-      }
-    }
 
-    return 0;
- }
- 
- 
- void minvalue(node *root)
- {
-    while(root->left != NULL)
-    {
-    root = root->left;
-    }
-    cout<<"Minimum value = " <<root->data<<endl;
- }
-   
- void mirror(node *root)
- {
-    if(root == NULL)
-    {
-      return;
-    }
-    else
-    {
-      node *ptr;
-      mirror(root->left);
-      mirror(root->right);
-      ptr = root->left;
-      root->left = root->right;
-      root->right = ptr; 
-    }
- }
- 
- void display()
- {
-    cout<<endl<<"--- Inorder Traversal ---"<<endl;
-    inorder(root);
-    cout<<endl;
-    cout<<endl<<"--- Postorder Traversal ---"<<endl;
-    postorder(root);
-    cout<<endl;
-    cout<<endl<<"--- Preorder Traversal ---"<<endl;
-    preorder(root);
-    cout<<endl;
- }
- 
- void inorder(node *root)
- {
-    if(root != NULL)
-    {
-    inorder(root->left);
-    cout<<root->data<<"  ";
-    inorder(root->right);
-    }
- } 
- 
- void postorder(node *temp)
- {
-    if(temp != NULL)
-    {
-    postorder(temp->left);
-    postorder(temp->right);
-    cout<<temp->data<<" ";
-    }
- }
- 
- void preorder(node *temp)
- {
-    if(temp != NULL)
-    {
-    cout<<temp->data<<" ";
-    preorder(temp->left);
-    preorder(temp->right);
-    }
- } 
- 
- int depth(node *root)
- {
-    if(root == NULL) 
       return 0;
+  }
+  
+  
+  void minvalue(node *root)
+  {
+      while(root->left != NULL)
+      {
+      root = root->left;
+      }
+      cout<<"Minimum value = " <<root->data<<endl;
+  }
+    
+  void mirror(node *root)
+  {
+      if(root == NULL)
+      {
+        return;
+      }
+      else
+      {
+        node *ptr;
+        mirror(root->left);
+        mirror(root->right);
+        ptr = root->left;
+        root->left = root->right;
+        root->right = ptr; 
+      }
+  }
+  
+  void display()
+  {
+      cout<<endl<<"--- Inorder Traversal ---"<<endl;
+      inorder(root);
+      cout<<endl;
+      cout<<endl<<"--- Postorder Traversal ---"<<endl;
+      postorder(root);
+      cout<<endl;
+      cout<<endl<<"--- Preorder Traversal ---"<<endl;
+      preorder(root);
+      cout<<endl;
+  }
+  
+  void inorder(node *root)
+  {
+      if(root != NULL)
+      {
+      inorder(root->left);
+      cout<<root->data<<"  ";
+      inorder(root->right);
+      }
+  } 
+  
+  void postorder(node *temp)
+  {
+      if(temp != NULL)
+      {
+      postorder(temp->left);
+      postorder(temp->right);
+      cout<<temp->data<<" ";
+      }
+  }
+  
+  void preorder(node *temp)
+  {
+      if(temp != NULL)
+      {
+      cout<<temp->data<<" ";
+      preorder(temp->left);
+      preorder(temp->right);
+      }
+  } 
+  
+  int depth(node *root)
+  {
+      if(root == NULL) 
+        return 0;
 
-    return (max((depth(root->left)),(depth(root->right))) +1);  
- }
+      return (max((depth(root->left)),(depth(root->right))) +1);  
+  }
 };
 
 int main()
