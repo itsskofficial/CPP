b5bb2b9fa27e11ed3d6f626d7ca4feffe27230cf
SK <sarthakkarandikar03@gmail.com>
2023-05-31 15:51:55 +0000

    May 31, 2023, 9:21 PM

--- data-structures/dictionaryHashing.py
+++ data-structures/dictionaryHashing.py
@@ -94,6 +94,7 @@ while choice!=4:
         chain_type=int(input("Enter 0 for chaining without replacement and 1 for chaining with replacement: "))
         d.insert(ele,chain_type)
     elif choice==2:
+        key
         d.delete(ele)
     elif choice==3:
         key=int(input("Enter key to find: "))
