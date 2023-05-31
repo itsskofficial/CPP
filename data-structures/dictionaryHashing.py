2fa7d8f482d5dc4e7faffc75c0c72c959941482a
SK <sarthakkarandikar03@gmail.com>
2023-05-31 15:23:03 +0000

    May 31, 2023, 8:53 PM

--- data-structures/dictionaryHashing.py
+++ data-structures/dictionaryHashing.py
@@ -7,6 +7,7 @@ class Dictionary:
         flag=0
         for i in range(len(self.elements)):
             if self.elements[i]['key']==0:
+                flag=
         pos=ele['key']%len(self.elements)
         if self.elements[pos]['key']==0:
             self.elements[pos]=ele
