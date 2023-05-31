fd4cd8b58764794b91b88d0ab1514ae92add940b
SK <sarthakkarandikar03@gmail.com>
2023-05-31 16:41:36 +0000

    May 31, 2023, 10:11 PM

--- data-structures/dictionaryHashing.py
+++ data-structures/dictionaryHashing.py
@@ -97,7 +97,7 @@ class Dictionary:
                     self.chain[prev]=next
                     self.chain[i]=-1
                     if i in self.chain_index:
-                        self.chain_index[temp['key']%len(self.elements)]==j
+                        self.chain_index[]==j
                     flag=1
         if flag==1:
             print("Element deleted successfully")
