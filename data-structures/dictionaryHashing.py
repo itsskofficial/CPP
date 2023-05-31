613e8e8295ee50a1c599816dcd52bf6dd150e854
SK <sarthakkarandikar03@gmail.com>
2023-05-31 15:45:17 +0000

    May 31, 2023, 9:15 PM

--- data-structures/dictionaryHashing.py
+++ data-structures/dictionaryHashing.py
@@ -67,7 +67,7 @@ class Dictionary:
                             count+=1
 
                         if count==len(self.elements):
-                            print("")
+                            print("Hash table full")
                         self.elements[current]=temp
 
                         if self.chain[pos]==-1:
