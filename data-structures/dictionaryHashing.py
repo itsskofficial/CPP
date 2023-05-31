1cfea748d4360c68454cc1db3f0edfb4558677c4
SK <sarthakkarandikar03@gmail.com>
2023-05-31 15:23:08 +0000

    May 31, 2023, 8:53 PM

--- data-structures/dictionaryHashing.py
+++ data-structures/dictionaryHashing.py
@@ -6,8 +6,7 @@ class Dictionary:
     def insert(self,ele, chain_type):
         flag=0
         for i in range(len(self.elements)):
-            if self.elements[i]['key']==0:
-                flag=
+            if self.elements[i]['key']!=0:
         pos=ele['key']%len(self.elements)
         if self.elements[pos]['key']==0:
             self.elements[pos]=ele
