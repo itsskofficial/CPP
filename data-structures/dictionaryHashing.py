8b0cb6ea95ae414e05d87c7dd0a9ba456dfb6f57
SK <sarthakkarandikar03@gmail.com>
2023-05-31 15:09:50 +0000

    May 31, 2023, 8:39 PM

--- data-structures/dictionaryHashing.py
+++ data-structures/dictionaryHashing.py
@@ -24,8 +24,8 @@ choice=0
 
 
 while choice!=4:
-    choice=int(input("What do you want to do?\n1. Insert a word\n2. Delete a word\n3. Find a word\n4. Exit\n"))
+    choice=int(input("What do you want to do?\n1. Insert a word\n2. Delete a word\n3. Find a word\n4. Exit\nEnter option number: "))
     if choice==1:
-
+        
         
 
