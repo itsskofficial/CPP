70c303026a97d1643445141962c896c9e5d272f6
SK <sarthakkarandikar03@gmail.com>
2023-05-31 15:22:14 +0000

    May 31, 2023, 8:52 PM

--- data-structures/dictionaryHashing.py
+++ data-structures/dictionaryHashing.py
@@ -5,6 +5,7 @@ class Dictionary:
 
     def insert(self,ele, chain_type):
         flag=0
+        
         pos=ele['key']%len(self.elements)
         if self.elements[pos]['key']==0:
             self.elements[pos]=ele
