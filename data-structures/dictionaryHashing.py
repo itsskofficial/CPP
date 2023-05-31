6dd589ad0db2db22cac3747df82780cf6581201a
SK <sarthakkarandikar03@gmail.com>
2023-05-31 15:10:25 +0000

    May 31, 2023, 8:40 PM

--- data-structures/dictionaryHashing.py
+++ data-structures/dictionaryHashing.py
@@ -26,6 +26,6 @@ choice=0
 while choice!=4:
     choice=int(input("What do you want to do?\n1. Insert a word\n2. Delete a word\n3. Find a word\n4. Exit\nEnter option number: "))
     if choice==1:
-        
+        chain_t
         
 
