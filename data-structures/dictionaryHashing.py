25b001071c53fe02db8eff5874ee6f773e187348
SK <sarthakkarandikar03@gmail.com>
2023-05-31 15:36:22 +0000

    May 31, 2023, 9:06 PM

--- data-structures/dictionaryHashing.py
+++ data-structures/dictionaryHashing.py
@@ -22,6 +22,7 @@ class Dictionary:
                     self.elements[current]=ele
                     if self.chain[pos]==-1:
                         self.chain[pos]=current
+                        self.chain[]
                     else:
                         self.chain[self.chain_index[pos]]=current
                 else:
