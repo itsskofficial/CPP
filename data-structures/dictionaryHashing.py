201672dfded2b4ce63789b8a0db66fd8a71a23a3
SK <sarthakkarandikar03@gmail.com>
2023-05-31 10:33:47 +0000

    May 31, 2023, 4:03 PM

--- data-structures/dictionaryHashing.py
+++ data-structures/dictionaryHashing.py
@@ -9,7 +9,7 @@ class Dictionary:
 
 choice=0
 print("What do you want to do?")
-print("\n1. Insert a word\n2. Delete a word\n3. ")
+print("\n1. Insert a word\n2. Delete a word\n3. Find a word\n4. Exit")
 
 while choice!=4:
 
