7f57ba4ae4fcabb4408ca94fcfa7a7ba4c38247f
SK <sarthakkarandikar03@gmail.com>
2023-05-31 15:16:31 +0000

    May 31, 2023, 8:46 PM

--- data-structures/dictionaryHashing.py
+++ data-structures/dictionaryHashing.py
@@ -33,6 +33,6 @@ while choice!=4:
         value=int(input("Enter value: "))
         ele={'key':key,'value':value}
         chain_type=int(input("Enter 0 for chaining without replacement and 1 for chaining with replacement: "))
-        d.insert(ele)
+        d.insert(ele,chain_type)
         
 
