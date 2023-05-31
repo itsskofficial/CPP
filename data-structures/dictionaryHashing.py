b34abcbcfb48514485f332c35d3af2757d1bf590
SK <sarthakkarandikar03@gmail.com>
2023-05-31 11:02:33 +0000

    May 31, 2023, 4:32 PM

---
+++ data-structures/dictionaryHashing.py
@@ -0,0 +1,27 @@
+class Dictionary:
+    elements=[{'key':0, 'value':0 } * 10]
+    chain=[-1 * 10]
+    
+    def insert(self,ele):
+        pos=ele['key']%len(self.elements)
+        if self.elements[pos]['key']==0:
+            self.elements[pos]=ele
+        else:
+            while(self.elements[pos]['key']==0):
+                pos=pos+1
+            self.elements[pos]
+
+
+        
+
+
+
+choice=0
+
+
+while choice!=4:
+    print("What do you want to do?")
+    print("\n1. Insert a word\n2. Delete a word\n3. Find a word\n4. Exit")
+    if choice==1:
+        
+
