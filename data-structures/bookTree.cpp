2e595248acedeed3cd3d0092679108d14fe6497a
SK <sarthakkarandikar03@gmail.com>
2023-05-31 03:52:36 +0000

    May 31, 2023, 9:22 AM

--- data-structures/bookTree.cpp
+++ data-structures/bookTree.cpp
@@ -1,116 +1,139 @@
-/*C++ Program To read details of a book consists of chapters,
- *chapters consist of sections and sections consist of
- *subsections. Construct a tree and print the nodes.
- *Find the time and space requirements of your method.
- **/
-# include <iostream>
-# include <cstdlib>
-# include <string.h>
+//Q. A book consists of chapters, chapters consist of sections and sections consist of subsections.
+//Construct a tree and print the nodes. Find the time and space requirements of your method
+
+#include<bits/stdc++.h>
 using namespace std;
-/*
- * Node Declaration
- */
+
 struct node
 {
     char label[10];
     int ch_count;
-    struct node *child[10];
-}*root;
+    struct node*child[10];
+}* root;
 
-class BookTree
+class GT
 {
     public:
-        void create_tree();  
-        void display(node * r1);
-     
-        BookTree()
+        void create_tree();
+        void display(node*root);
+        GT()
         {
-            root = NULL;
+            root=NULL;
         }
 };
 
-void BookTree::create_tree()
+void GT::create_tree()
 {
- int tbooks,tchapters,i,j,k;
- root = new node;
- cout<<"Enter name of book";
- cin>>root->label;
- cout<<"Enter no. of chapters in book";
- cin>>tchapters; 
- root->ch_count = tchapters;
- for(i=0;i<tchapters;i++)
- {
-    root->child[i] = new node;
-    cout<<"Enter Chapter name\n";
-    cin>>root->child[i]->label;   
-    cout<<"Enter no. of sections in  Chapter: "<<root->child[i]->label;
-    cin>>root->child[i]->ch_count;
-    for(j=0;j<root->child[i]->ch_count;j++)
+    int i,j,k;
+    root=new node;
+    cout<<"Enter book name:";
+    cin>>root->label;
+    cout<<"Enter number of chapters:";
+    cin>>root->ch_count;
+    for (int i=0; i<root->ch_count; i++)
     {
-        root->child[i]->child[j] = new node;
-        cout<<"Enter Section "<<j+1<<"name\n";
-        cin>>root->child[i]->child[j]->label;   
-        //cout<<"Enter no. of subsections in "<<r1->child[i]->child[j]->label;
-        //cin>>r1->child[i]->ch_count;
-    }  
- }
-}
-
-
-void BookTree::display(node * r1)
-{
- int i,j,k,tchapters;
- if(r1 != NULL)
- { 
-  cout<<"\n-----Book Hierarchy---";
-
-  cout<<"\n Book title : "<<r1->label;
-  tchapters = r1->ch_count;
-  for(i=0;i<tchapters;i++)
-  {
-  
-   cout<<"\n  Chapter "<<i+1;
-   cout<<" "<<r1->child[i]->label;   
-    cout<<"\n Sections";
-   for(j=0;j<r1->child[i]->ch_count;j++)
-   {
-    //cin>>r1->child[i]->child[j]->label;   
-    cout<<"\n  "<<r1->child[i]->child[j]->label;
-   }  
-
-  }
- }
-}
-
-
-/*
- * Main Contains Menu
- */
-int main()
-{
-    int choice;
-   BookTree gt;
-    while (1)
-    {
-        cout<<"-----------------"<<endl;
-        cout<<"Book Tree Creation"<<endl;
-        cout<<"-----------------"<<endl;
-        cout<<"1.Create"<<endl;
-        cout<<"2.Display"<<endl;
-        cout<<"3.Quit"<<endl;
-        cout<<"Enter your choice : ";
-        cin>>choice;
-        switch(choice)
+        root->child[i]=new node;
+        cout<<"Enter name of chapter"<<i+1<<":";
+        cin>>root->child[i]->label;
+        cout<<"Enter number of sections:";
+        cin>>root->child[i]->ch_count;
+        for (int j=0; j<root->child[i]->ch_count; j++)
         {
-        case 1:
-              gt.create_tree();
-        case 2:
-              gt.display(root);
-            break;
-        case 3:
-            exit(1);
-        default:
-            cout<<"Wrong choice"<<endl;
+            root->child[i]->child[j]=new node;
+            cout<<"Enter name of Section:"<<j+1<<":";
+            cin>>root->child[i]->child[j]->label;
+            cout<<"Enter number of sub-sections:";
+            cin>>root->child[i]->child[j]->ch_count;
+            for (int k=0; k<root->child[i]->child[j]->ch_count; k++)
+            {
+                root->child[i]->child[j]->child[k]=new node;
+                cout<<"Enter name of Sub-Section:"<<k+1<<":";
+                cin>>root->child[i]->child[j]->label;
+            }
+        }
+    }    
+};
+
+void GT::display(node*root)
+{
+    int i,j,k;
+    if(root!=NULL)
+    {
+        
+        cout<<"\nName of the book:"<<root->label;
+        for (int i=0; i<root->ch_count; i++)
+        {
+            cout<<"\nThe chapter"<<i+1<<"name:"<<root->child[i]->label;
+            for (int j=0; j<root->child[i]->ch_count; j++)
+            {
+                cout<<"\nThe section"<<j+1<<"name:"<<root->child[i]->child[j]->label;
+            
+                
+                for (int k=0; k<root->child[i]->child[j]->ch_count; k++)
+                {
+                    cout<<"\nThe sub-sections"<<i+1<<"name:"<<root->child[i]->child[j]->label;
+                }
+            }
+
         }
     }
 }
+
+int main(){
+    int choice;
+    GT gt;
+    while(1){
+        cout<<"Welcome To Book Hierarchy!";
+        cout<<"Choose from the following:";
+        cout<<"\n1.Create\n2.Display\n3.Exit\n";
+        cin>>choice;
+        switch(choice)
+        {
+            case 1:
+            gt.create_tree();
+            break;
+
+            case 2:
+            gt.display(root);
+            break;
+
+            case 3:
+            exit(1);
+
+            default:
+            cout<<"Wrong choice";
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
