ddeda9e876c67658f86a14f143ff5ffbe86709dc
SK <sarthakkarandikar03@gmail.com>
2023-05-31 16:12:42 +0000

    May 31, 2023, 9:42 PM

--- data-structures/dictionaryHashing.py
+++ data-structures/dictionaryHashing.py
@@ -84,7 +84,7 @@ class Dictionary:
                                     self.chain_index[pos]=current
         def delete(self,key):
             for i in self.elements:
-                if 
+                if i['key']
 
 choice=0
 d=Dictionary()
