2256f8d341fd3e8956c2f25bdf0c8c0d518413d4
SK <sarthakkarandikar03@gmail.com>
2023-05-31 03:52:45 +0000

    May 31, 2023, 9:22 AM

--- data-structures/bookTree.cpp
+++ data-structures/bookTree.cpp
@@ -1,6 +1,3 @@
-//Q. A book consists of chapters, chapters consist of sections and sections consist of subsections.
-//Construct a tree and print the nodes. Find the time and space requirements of your method
-
 #include<bits/stdc++.h>
 using namespace std;
 
