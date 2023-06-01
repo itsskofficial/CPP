7af9ebce7eef488892fb62bdd1ee95830aa2cb23
SK <sarthakkarandikar03@gmail.com>
2023-06-01 03:59:40 +0000

    Jun 1, 2023, 9:29 AM

--- data-structures/telephoneBook.py
+++ data-structures/telephoneBook.py
@@ -10,9 +10,9 @@ class Hashtable:
 		if (self.table[pos]!=0):
 			self.table[pos]=x
 		else:
-			if probe_type==0:
-				count=1
-				current=pos+1
+            if probe_type==0:
+                count=1
+                current=pos+1
                 while self.table[current]!=0 and count!=10:
                     current=(current+1)%10
                     count+=1
