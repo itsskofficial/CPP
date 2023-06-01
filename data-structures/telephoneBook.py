4dcabbe6998b89b9e384fa4e8d443e34fb9e6b04
SK <sarthakkarandikar03@gmail.com>
2023-06-01 03:54:44 +0000

    Jun 1, 2023, 9:24 AM

--- data-structures/telephoneBook.py
+++ data-structures/telephoneBook.py
@@ -3,7 +3,7 @@ n= int(input("Enter number of clients: "))
 
 class Hashtable:
 	def __init__(self):
-		self.table[10]=[null]
+		self.table[10]=[0] * 10
 		self.pos=null
 	def insert_element(x):
 		pos=x%10
