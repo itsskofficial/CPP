ad6c142af9024f322257f95c9478c8af9fc45ae2
SK <sarthakkarandikar03@gmail.com>
2023-11-11 06:14:37 +0000

    Nov 11, 2023, 11:44 AM

---
+++ Leetcode/generateParantheses.cpp
@@ -0,0 +1,25 @@
+class Solution 
+{
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) 
+        {
+            vector<int> ans;
+            for (int i = 0; i <= nums.size() - 2; i++)
+            {
+                for (int j = i + 1; j <= nums.size() - 1; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        ans.push_back(i);
+                        ans.push_back(j);
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    } 
+                }
+            }
+            return ans;
+        }
+};
