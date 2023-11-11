b6493a7f409171bf93b7e0943ba93503eb7fb896
SK <sarthakkarandikar03@gmail.com>
2023-11-11 06:47:05 +0000

    Nov 11, 2023, 12:17 PM

---
+++ Leetcode/Solution.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/remove-element
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) 
+    {
+        int k = 0;
+        
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[k] = nums[i];
+                k += 1;
+            }
+        }
+
+        return k;
+    }
+};
