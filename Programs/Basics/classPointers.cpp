56204c3e5dc19ac804789045152dee0e7b1b3dfc
Abhijeet <abhijeetsuryawanshi128@gmail.com>
2024-03-04 13:58:24 +0530

    rework

---
+++ Programs/Basics/classPointers.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Person
+{
+    public:
+        string name;
+        int age;  
+
+    void show()
+    {
+        cout << name << endl;
+        cout << age << endl;
+    }
+
+    void isEligible()
+    {
+        if (age >= 18)
+            cout << name << "is eligible for driving";
+        else 
+            cout << name << " is not eligible for driving";
+    }
+};
+
+int main()
+{
+    Person *p1 = new Person();
+    p1->name = "Sarthak";
+    p1->age = 10;
+    p1->show();
+    p1->isEligible();
+
+    return 0;
+}
